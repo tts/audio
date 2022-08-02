8976f8f77c92ddcc114c1d5a61f34af313f5190a
Tuija <tuija.sonkkila@aalto.fi>
2022-08-02 20:33:37 +0300

    Initial commit

---
+++ potoo.R
@@ -0,0 +1,9 @@
+library(av)
+
+url_potoo_audio <- "https://upload.wikimedia.org/wikipedia/commons/1/12/Great_Potoo_2007-03-26_2100.ogg"
+download.file(url_potoo_audio, "potoo.ogg", mode = "wb")
+
+av_spectrogram_video('potoo.ogg', 
+                     output = 'spectrogram_potoo.mp4', 
+                     width = 1280, height = 720, res = 144)
+
